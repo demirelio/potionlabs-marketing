730bea36f5e0f5567d1cd7862c68b06a181330cf
Emre Demirel <emre@emre.local>
2022-04-11 10:49:34 +0200

    typo

--- src/components/cta.js
+++ src/components/cta.js
@@ -5,7 +5,7 @@ export default function CTA() {
   return (
     <section className="max-w-5xl py-[36px] px-[24px] lg:px-[48px] lg:pt-20 lg:mx-auto grid gap-8 lg:min-w-[1024px]">
       <h1 className="font-jakarta800 text-white text-3xl md:text-4xl lg:text-5xl xl:text-6xl tracking-wide lg:leading-tight xl:leading-tight">
-        Contact and see if Potion Mail is for you. What are you waiting for?
+        Contact and see if Potion Labs is for you. What are you waiting for?
       </h1>
       <div className="lg:max-w-3xl">
         <div className="flex gap-4 items-center justify-start">
